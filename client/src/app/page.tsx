dbc848d6253e316e6c65416ef32620ae90b2b96f
nabila <nabilakiswanto@gmail.com>
2025-03-18 11:20:48 +0800

    use the blockrenderer

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import { getHomePage } from "@/data/loaders";
 import { notFound } from "next/navigation";
-import { HeroSection } from "@/components/blocks/HeroSection";
+import { BlockRenderer } from "@/components/BlockRenderer";
 async function loader(){
  const data = await getHomePage();
  if(!data) notFound();
@@ -14,15 +12,5 @@ export default async function HomeRoute() {
   const data = await loader();
   const blocks = data?.blocks || [];
 
-  return (
-    <div>
-      {/* <h1>
-        {data.title}
-      </h1>
-      <p>
-        {data.description}
-      </p> */}
-      <HeroSection {...blocks[0]}/>
-    </div>
-  );
+  return <BlockRenderer blocks={blocks}/>
 }
