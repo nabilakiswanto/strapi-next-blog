b707c1c782f67eabc66a716b84c09d4f99677927
nabila <nabilakiswanto@gmail.com>
2025-03-24 11:51:25 +0800

    add event pages with signup event form

--- client/src/types.ts
+++ client/src/types.ts
@@ -146,4 +146,20 @@ export interface FullImageProps extends Base<"blocks.full-image"> {
   __component: "blocks.full-image";
   image: ImageProps;
 }
+
+export interface EventProps {
+  id: number;
+  documentId: string;
+  title: string;
+  description: string;
+  slug: string;
+  image: ImageProps;
+  author: string;
+  featured: boolean;
+  price: string;
+  startDate: string;
+  publishedAt: string;
+  createdAt: string;
+  updatedAt: string;
+}
   
