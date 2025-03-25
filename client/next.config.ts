b38861c85a36410f070aa8d95b43d25208df2b12
nabila <nabilakiswanto@gmail.com>
2025-03-25 10:46:07 +0800

    update next config

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,17 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images:{
-    remotePatterns:[
+  images: {
+    remotePatterns: [
       {
         protocol: "http",
         hostname: "localhost",
-        port:"1337",
-        pathname:"/uploads/**/*",
-      }
-    ]
-  }
+        port: "1337",
+        pathname: "/uploads/**/*",
+      },
+      {
+        protocol: "https",
+        hostname: "blog.npk.web.id",
+        pathname: "/uploads/**/*",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
